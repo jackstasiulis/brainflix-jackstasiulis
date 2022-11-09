de51ba98c60a653740bb448890d0145385f826d3
Jack <jackstasiulis@gmail.com>
2022-11-08 20:21:47 -0800

    clean up code on home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,9 +3,6 @@ import PlayerDetails from '../../Components/PlayerDetails/PlayerDetails';
 import Conversation from '../../Components/Conversation/Conversation'
 import NextVids from '../../Components/NextVids/NextVids'
 
-
-// import videoDetailsMain from '../../assets/Data/video-details.json'
-// import videoList from '../../assets/Data/videos.json';
 import { useState } from 'react';
 import { dateParser } from '../../Utilities/DateParser';
 
@@ -16,18 +13,10 @@ import axios from "axios";
 function Home() {
 
     const[activeVid, setActiveVid] = useState([]);
-    // const[activeVid, setActiveVid] = useState([videoDetailsMain[0]]);
-  
     const[videoListDetails, setVideoListDetails] = useState([]);
-    // const[videoListDetails, setVideoListDetails] = useState(videoList);
 
-    // const updateActiveVid = (id) => {
-    //   const foundVid = (videoDetailsMain.find((video) => video.id === id))
-    //   setActiveVid(foundVid)
-    // }
-
+ 
     const {id} = useParams();
-    // const [videoDataMain, setVideoDataMain] = useState([]);
     
     useEffect(()=> {
       console.log('video main mounted?');
@@ -53,14 +42,6 @@ function Home() {
           setVideoListDetails(response.data)
         }).catch((error) => console.log(error));
     }, []);
-    // console.log(activeVid);
-
-
-    // 0e5174a0-d772-40a4-9218-ad902e35564f
-
-
-// this is for the small vids list
-// https://project-2-api.herokuapp.com/videos/?api_key=<0e5174a0-d772-40a4-9218-ad902e35564f>
   
     return (
       <main>
@@ -81,20 +62,3 @@ function Home() {
   }
   
   export default Home;
-
-  // return (
-  //   <main>
-
-  //       <Player activeVid = {activeVid.image}/>
-  //   <div className='desktopSplit'>
-  //       <div className='desktopSplit__left'>
-  //         <PlayerDetails activeVid = {activeVid} updateActiveVid = {updateActiveVid} dateParser = {dateParser} />
-  //         <Conversation activeVid = {activeVid} updateActiveVid = {updateActiveVid} dateParser = {dateParser} />
-  //       </div>
-  //       <div className='desktopSplit__right'>
-  //         <NextVids videoListDetails = {videoListDetails} updateActiveVid = {updateActiveVid} activeVid = {activeVid} />
-  //       </div>
-  //   </div>
-
-  //   </main>
-  // );
